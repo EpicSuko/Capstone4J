9056b285330c8338e25b2fea466b8a3d7b87c76b
Suko <sukohinken@gmail.com>
2025-03-20 23:44:03 -0400

    Add CapstoneGroup enum for instruction group representation
    - Introduced CapstoneGroup enum to define various instruction groups with associated values.
    - Implemented methods for retrieving the value of each group and for converting from value to enum type.
    - This addition enhances the structure and usability of instruction group handling in the disassembly process.

---
+++ app/src/main/java/com/capstone4j/CapstoneGroup.java
@@ -0,0 +1,34 @@
+package com.capstone4j;
+
+import static com.capstone4j.internal.capstone_h.*;
+
+public enum CapstoneGroup {
+
+    INVALID(CS_GRP_INVALID()),
+	JUMP(CS_GRP_JUMP()),
+	CALL(CS_GRP_CALL()),
+	RET(CS_GRP_RET()),
+	INT(CS_GRP_INT()),
+	IRET(CS_GRP_IRET()),
+	PRIVILEGE(CS_GRP_PRIVILEGE()),      
+	BRANCH_RELATIVE(CS_GRP_BRANCH_RELATIVE());
+
+    private final int value;
+
+    CapstoneGroup(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public static CapstoneGroup fromValue(int value) {
+        for(CapstoneGroup group : CapstoneGroup.values()) {
+            if(group.getValue() == value) {
+                return group;
+            }
+        }
+        return INVALID;
+    }
+}
